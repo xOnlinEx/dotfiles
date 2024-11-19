2a704f7a8418cdbf57ccf6c0d4f124c9bc9f2637
xOnlinEx <daniel.floresvargas666@gmail.com>
2024-11-19 08:53:15 -0400

    add: drivers amd

---
+++ drivers.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+paru -S xf86-video-amdgpu vulkan-radeon mesa-libgl mesa-vdpau libvdpau-va-gl libva-mesa-driver
+# Alsa y pipewire para manejar el audio
+# sudo pacman -S pipewire pipewire-alsa pipewire-pulse alsa-utils
