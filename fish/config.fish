5541354554cf386c88c8543de099a9f8bf761ed9
xOnlinEx <daniel.floresvargas666@gmail.com>
2025-03-12 21:34:09 -0400

    add: new config fish

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Remove default message fish_shell
 set fish_greeting
 
 abbr --add g git
@@ -9,7 +10,7 @@ abbr --add gcl git clone
 abbr --add gst git status
 abbr --add p python
 
-# aliases
+# Aliases
 alias ls='eza --icons'
 alias ll='eza --icons -l'
 alias la='eza --icons -la'
@@ -17,32 +18,35 @@ alias lt='eza --icons -T'
 alias nv='nvim'
 alias z='zoxide'
 alias cat=bat
+alias paru='paru --limit 5'
 
 # ASDF configuration code
-if test -z $ASDF_DATA_DIR
-    set _asdf_shims "$HOME/.asdf/shims"
-else
-    set _asdf_shims "$ASDF_DATA_DIR/shims"
-end
+# if test -z $ASDF_DATA_DIR
+#     set _asdf_shims "$HOME/.asdf/shims"
+# else
+#     set _asdf_shims "$ASDF_DATA_DIR/shims"
+# end
 
-if not contains $_asdf_shims $PATH
-    set -gx --prepend PATH $_asdf_shims
-end
-set --erase _asdf_shims
+# if not contains $_asdf_shims $PATH
+#     set -gx --prepend PATH $_asdf_shims
+# end
+# set --erase _asdf_shims
 
 # JAVA_HOME
-. ~/.asdf/plugins/java/set-java-home.fish
+# . ~/.asdf/plugins/java/set-java-home.fish
 
-# starship
+# Starship
 starship init fish | source
 set -x STARSHIP_CONFIG ~/.config/starship/starship.toml
 
+# Zoxide
 zoxide init fish | source
 
-# init with zellij
-if set -q ZELLIJ
-else
-    zellij
-end
+# Init with zellij
+# if set -q ZELLIJ
+# else
+#   zellij
+# end
 
-set -x DISPLAY 172.31.64.1:0.0
+# GUI xserver for wsl2
+# set -x DISPLAY CHANGE_FOR_IP_WSL2:0.0
