7e5b6759fbb676553e2320c5af9f5297824fa98f
xOnlinEx <daniel.floresvargas666@gmail.com>
2025-10-31 18:16:39 -0400

    add config fish shell

---
+++ fish/conf.d/abbreviations.fish
@@ -0,0 +1,8 @@
+# Git
+abbr --add g git
+abbr --add gcl git clone
+abbr --add gst git status
+
+# Python
+abbr --add p python
+
---
+++ fish/conf.d/aliases.fish
@@ -0,0 +1,11 @@
+# Eza (ls replacements)
+alias la="eza --icons -la"
+alias ll="eza --icons -l"
+alias ls="eza --icons"
+alias lt="eza --icons -T"
+
+# Bat
+alias cat=bat
+
+# Paru
+alias paru="paru --limit 5"
