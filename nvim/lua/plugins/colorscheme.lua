38df74f38eae30b950ac6ba740c95961af6bc983
xOnlinEx <daniel.floresvargas666@gmail.com>
2025-10-31 18:19:57 -0400

    add config nvim

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "Gentleman-Programming/gentleman-kanagawa-blur",
+    name = "gentleman-kanagawa-blur",
+    priority = 1000,
+    opts = {},
+  },
+  {
+    "datsfilipe/vesper.nvim",
+  },
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "vesper",
+    },
+  },
+}
