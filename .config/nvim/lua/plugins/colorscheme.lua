b4c8c04ba7a74d55f4bdd341ab2757a9361d8869
xOnlinEx <daniel.floresvargas666@gmail.com>
2025-11-03 15:07:01 -0400

    Init config with STOW

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,38 @@
+return {
+  {
+    "Gentleman-Programming/gentleman-kanagawa-blur",
+    name = "gentleman-kanagawa-blur",
+    priority = 1000,
+    opts = {},
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      background = {
+        light = "latte",
+        dark = "mocha",
+      },
+      transparent_background = true,
+      float = {
+        transparent = true,
+        solid = false,
+      },
+    },
+  },
+  {
+    'Mofiqul/vscode.nvim',
+    opts = {
+      transparent = true,
+      italic_comments = true,
+    }
+  },
+  -- Configure LazyVim for colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
